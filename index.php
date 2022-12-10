cc08d757d396546df5a534f7454cd22b28950cff
FADZ <89492120+FADZ30@users.noreply.github.com>
2022-12-10 18:45:00 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,5 +2,5 @@
     include ("config.php");
     $var1 = $_GET['data1'];
     $var2 = $_GET['data2'];
-    mysqli_query($koneksi, "INSERT INTO sensor(NIM,Pass) VALUES('$var1','$var2')");
+    mysqli_query($koneksi, "INSERT INTO user(NIM,Pass) VALUES('$var1','$var2')");
 ?>
